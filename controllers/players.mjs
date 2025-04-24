484540d3e630e419ca851f118f78a0ab02593771
awatersny <awatersny@gmail.com>
2025-04-24 11:51:38 -0400

    Create seed route for players

--- controllers/players.mjs
+++ controllers/players.mjs
@@ -3,7 +3,38 @@ import Player from "../models/player.mjs";
 export async function getAllPlayers(req, res){
   try {
     const players = await Player.find()
-    res.status(200).json([players, {route: "players"}])
+    if(players.length === 0) res.status(200).json({route: "players"})
+    else res.status(200).json(players)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export async function generatePlayers (req, res) {
+  try {
+    await Player.create([
+      {
+        name: "Spice",
+        splash_tag: "1981",
+        role: "Slayer"
+      },
+      {
+        name: "ToriiStars",
+        splash_tag: "1123",
+        role: "Support"
+      },
+      {
+        name: "Sasu",
+        splash_tag: "1542",
+        role: "Anchor",
+      },
+      {
+        name: "Mobes",
+        splash_tag: "2478",
+        role: "Skirmisher"
+      }
+    ])
+    res.redirect('/api/players')
   } catch (error) {
     console.error(error)
   }
