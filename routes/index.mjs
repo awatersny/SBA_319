ce13084e77a1cc6131f297ff792ce650fcd53972
awatersny <awatersny@gmail.com>
2025-04-26 14:21:53 -0400

    Update HATEOAS links

--- routes/index.mjs
+++ routes/index.mjs
@@ -3,108 +3,110 @@ const router = express.Router()
 
 router.get("/", (req, res) => {
   res.json({
-    message: "Welcome to the API",
+    title: "Splatoon Tournament API",
+    apiLinks: "/api",
     calloutsRenderedAt: "/callouts",
-    renderedCalloutsById: "/callouts/:id",
-    apiLinks: [
+    renderedCalloutsById: "/callouts/:id"
+  })
+})
+
+router.get("/api", (req, res) => {
+  res.json({
+    links: [
       {
-        href: "/api/players",
+        href: "/players",
         rel: "players",
         type: "GET"
       },
       {
-        href: "/api/players",
+        href: "/players",
         rel: "players",
         type: "POST"
       },
       {
-        href: "/api/players/:id",
+        href: "/players/:id",
         rel: "players",
         type: "GET"
       },
       {
-        href: "/api/players/:id",
+        href: "/players/:id",
         rel: "players",
         type: "PUT"
       },
       {
-        href: "/api/players/:id",
+        href: "/players/:id",
         rel: "players",
         type: "DELETE"
       },
       {
-        href: "/api/teams",
+        href: "/teams",
         rel: "teams",
         type: "GET"
       },
       {
-        href: "/api/teams",
+        href: "/teams",
         rel: "teams",
         type: "POST"
       },
       {
-        href: "/api/teams/:id",
+        href: "/teams/:id",
         rel: "team",
         type: "GET"
       },
       {
-        href: "/api/teams/:id",
+        href: "/teams/:id",
         rel: "team.name",
         type: "PATCH"
       },
       {
-        href: "/api/teams/:id",
+        href: "/teams/:id",
         rel: "team",
         type: "DELETE"
       },
       {
-        href: "/api/teams/:id/members",
+        href: "/teams/:id/members",
         rel: "team.players",
         type: "GET"
       },
       {
-        href: "/api/teams/:id/members",
+        href: "/teams/:id/members",
         rel: "team.players",
         type: "POST"
       },
       {
-        href: "/api/teams/:id/members/:playerId",
+        href: "/teams/:id/members/:playerId",
         rel: "team.players",
         type: "GET"
       },
       {
-        href: "/api/teams/:id/members/:playerId",
+        href: "/teams/:id/members/:playerId",
         rel: "team.players",
         type: "DELETE"
       },
       {
-        href: "/api/mapmodes",
+        href: "/mapmodes",
         rel: "mapmodes",
         type: "GET"
       }
     ],
     seedLinks: [
       {
-        href: "/api/teams/seed",
+        href: "/teams/seed",
         rel: "teams",
         type: "GET"
       },
       {
-        href: "/api/players/seed",
+        href: "/players/seed",
         rel: "players",
         type: "GET"
       },
       {
-        href: "/api/mapmodes/seed",
+        href: "/mapmodes/seed",
         rel: "mapmodes",
         type: "GET"
       }
     ]
   })
-})
-
-router.get("/api", (req, res) => {
-  res.redirect("/")
 })
 
 export default router
