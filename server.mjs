c5c6ee8ad304f8b3606b13406a8d66e320cbe715
awatersny <awatersny@gmail.com>
2025-04-26 00:00:16 -0400

    Add error handling middleware

--- server.mjs
+++ server.mjs
@@ -4,14 +4,13 @@ import mongoose from "mongoose"
 import mapModes from "./routes/mapModes.mjs"
 import players from "./routes/players.mjs"
 import teams from "./routes/teams.mjs"
+import index from "./routes/index.mjs"
+import callouts from "./routes/callouts.mjs"
+import error from "./utilities/error.mjs"
 
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT
-
-app.use(express.static("./assets"))
-app.use(express.urlencoded())
-app.use(express.json())
 
 mongoose.connect(`${process.env.ATLAS_URI}/SBA_319`)
 
@@ -19,13 +18,45 @@ mongoose.connection.once("open", () => {
   console.log(`Connected to the ${mongoose.connection.name} database.`)
 })
 
+app.use(express.static("./assets"))
+app.use(express.urlencoded())
+app.use(express.json())
+
+app.use((req, res, next) => {
+  const time = new Date()
+  console.log("-----")
+  if(req.url !== "/favicon.ico") {
+    console.log(`${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`)
+  }
+  if(req.body){
+    if (Object.keys(req.body).length > 0) {
+      console.log("Containing the data:")
+      console.log(req.body)
+    }
+  }
+  next();
+});
+
+app.set("views", "./views")
+app.set("view engine", "ejs")
+
+app.use("/", index)
+app.use("/callouts", callouts)
 app.use("/api/mapmodes", mapModes)
 app.use("/api/players", players)
 app.use("/api/teams", teams)
 
-app.use("/", (req, res) => {
-  res.send({message: "Welcome to the API"})
-})
+app.use((req, res, next) => {
+  next(error(404, "Oops!  There's nothing here."));
+});
+
+app.use((err, req, res, next) => {
+  if(req.url !== "/favicon.ico") {
+    console.log(`${err.status} Not found.`)
+  }
+  res.status(err.status || 500);
+  res.json({ error: `${err.status} ${err.message}` });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is open at localhost:${PORT}.`)
