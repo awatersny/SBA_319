c74f0298349daa44caec12d2f09ec06705f7371e
awatersny <awatersny@gmail.com>
2025-04-25 23:51:29 -0400

    Add some HATEOAS links

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,8 +1,32 @@
 import express from "express"
 const router = express.Router()
 
+router.get("/api", (req, res) => {
+  res.redirect("/")
+})
+
 router.get("/", (req, res) => {
-  res.json({message: "Welcome to the API"})
+  res.json({
+    message: "Welcome to the API",
+    calloutsRenderedAt: "/callouts",
+    links: [
+      {
+        href: "/api/plavers",
+        rel: "players",
+        type: "GET"
+      },
+      {
+        href: "/api/teams",
+        rel: "teams",
+        type: "GET"
+      },
+      {
+        href: "/api/mapmodes",
+        rel: "mapmodes",
+        type: "GET"
+      }
+    ]
+  })
 })
 
 export default router
